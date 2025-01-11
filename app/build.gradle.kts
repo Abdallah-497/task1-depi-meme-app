10787d08f50abaecfbf5d374fc62a6ca6d25cb6b
abdallah <abdollahtaha73@gmail.com>
2025-01-11 21:32:09 +0200

    First Commit upload project

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.abdallah.firstsession"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.abdallah.firstsession"
